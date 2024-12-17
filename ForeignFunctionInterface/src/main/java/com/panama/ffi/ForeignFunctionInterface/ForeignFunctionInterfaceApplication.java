79e1010ea9dcd993b743e81e3769e651cc7337a6
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-12-17 14:56:43 +0530

    init

---
+++ ForeignFunctionInterface/src/main/java/com/panama/ffi/ForeignFunctionInterface/ForeignFunctionInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.panama.ffi.ForeignFunctionInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignFunctionInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignFunctionInterfaceApplication.class, args);
+	}
+
+}
---
+++ ForeignFunctionInterface/src/test/java/com/panama/ffi/ForeignFunctionInterface/ForeignFunctionInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panama.ffi.ForeignFunctionInterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignFunctionInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
